9998dfd291f0ade41fe8b9271b2ffc3485ad31f0
Zach Drummond <zachdrummond3@gmail.com>
2021-10-20 14:32:31 -0400

    Added to Reviews.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./Containers/Footer/Footer"
 import Navigation from "./Containers/Navigation/Navigation";
+import Reviews from "./Components/Reviews/Reviews.jsx";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Reviews />
       <Footer />
     </>
   );
