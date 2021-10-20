9b9795b1b5ec7f4dd6dab084fec37bb676955632
Zach Drummond <zachdrummond3@gmail.com>
2021-10-20 14:20:15 -0400

    Added Reviews.js

---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,36 @@
+const REVIEWS = {
+      // Customer Reviews
+   customers : [
+    {
+      name: "Person 1",
+      feedback:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, aliquid! Quae maxime consectetur eligendi dolor sunt doloremque corrupti, assumenda excepturi facilis numquam quam, officiis, delectus fuga reiciendis animi ipsa quisquam!",
+    },
+    {
+      name: "Person 2",
+      feedback:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. At fugiat perspiciatis, eveniet repellendus doloremque suscipit ut nobis facilis ex neque officia quo rerum, laborum magnam! Magni quo accusantium asperiores reprehenderit?",
+    },
+    {
+      name: "Person 3",
+      feedback:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia itaque odio praesentium consequuntur temporibus ea architecto quidem est ad maiores veniam pariatur, dolores recusandae nostrum doloribus ratione nam voluptatem quo!",
+    },
+    {
+      name: "Person 4",
+      feedback:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, esse? Facilis, voluptate dolores! Deserunt vero itaque autem iste illum explicabo aliquam, rem molestiae optio eos, eveniet ex. Laudantium, incidunt molestias.",
+    },
+    {
+      name: "Person 5",
+      feedback:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum explicabo hic possimus, blanditiis ab sapiente libero impedit error cupiditate consequuntur natus placeat sint expedita sequi fugiat necessitatibus temporibus? Optio, ratione?",
+    },
+    {
+      name: "Person 6",
+      feedback:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis fuga itaque unde sunt rerum quia explicabo quod aspernatur saepe beatae animi dicta, nemo voluptatibus. Incidunt minima nemo tempora debitis perspiciatis.",
+    },
+  ],
+}
+export default REVIEWS;
