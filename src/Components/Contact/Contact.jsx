356b4b1e7da2ac3add7fc77fa49dbef4fdc1dee4
Zach Drummond <zachdrummond3@gmail.com>
2021-10-21 15:27:29 -0400

    Added required to all forms

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,11 @@
-import { Col, Container, Form, FloatingLabel, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FloatingLabel,
+  Row,
+} from "react-bootstrap";
 
 const Contact = () => {
   return (
@@ -7,44 +14,50 @@ const Contact = () => {
         <Row>
           <Col>
             <FloatingLabel label="First and Last Name">
-              <Form.Control type="text" placeholder="First Name Last Name" />
+              <Form.Control
+                type="text"
+                placeholder="First Name Last Name"
+                required
+              />
             </FloatingLabel>
           </Col>
           <Col>
             <FloatingLabel label="Phone Number" className="mb-3">
-              <Form.Control type="number" placeholder="XXX-XXX-XXXX" />
+              <Form.Control type="number" placeholder="XXX-XXX-XXXX" required />
             </FloatingLabel>
           </Col>
         </Row>
         <FloatingLabel label="Email Address" className="mb-3">
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control type="email" placeholder="name@example.com" required />
         </FloatingLabel>
         <FloatingLabel label="Address" className="mb-3">
-          <Form.Control type="text" placeholder="1234 Main St" />
+          <Form.Control type="text" placeholder="1234 Main St" required />
         </FloatingLabel>
         <Row>
           <Col md={6}>
             <FloatingLabel label="City" className="mb-3">
-              <Form.Control type="text" placeholder="City Name" />
+              <Form.Control type="text" placeholder="City Name" required />
             </FloatingLabel>
           </Col>
           <Col md={4}>
             <FloatingLabel label="State" className="mb-3">
-              <Form.Control type="text" placeholder="State Name" />
+              <Form.Control type="text" placeholder="State Name" required />
             </FloatingLabel>
           </Col>
           <Col md={2}>
             <FloatingLabel label="Zip Code" className="mb-3">
-              <Form.Control type="text" placeholder="Zip Code" />
+              <Form.Control type="text" placeholder="Zip Code" required />
             </FloatingLabel>
           </Col>
         </Row>
-        <FloatingLabel label="Description">
+        <FloatingLabel label="Description" className="mb-3">
           <Form.Control
             as="textarea"
             placeholder="Briefly Describe Your Project"
+            required
           />
         </FloatingLabel>
+        <Button type="submit">Submit form</Button>
       </Form>
     </Container>
   );
