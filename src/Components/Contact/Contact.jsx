0f27fdb7b5cedc1acc658876c58002d2d80f4df1
Zach Drummond <zachdrummond3@gmail.com>
2021-10-21 15:19:34 -0400

    Created better layout for Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,52 @@
-import { Form, FloatingLabel } from "react-bootstrap";
+import { Col, Container, Form, FloatingLabel, Row } from "react-bootstrap";
 
 const Contact = () => {
   return (
-    <>
-      <FloatingLabel label="First and Last Name" className="mb-3">
-        <Form.Control type="text" placeholder="First Name Last Name" />
-      </FloatingLabel>
-      <FloatingLabel label="Phone Number">
-        <Form.Control type="number" placeholder="XXX-XXX-XXXX" />
-      </FloatingLabel>
-      <FloatingLabel label="Phone Number">
-        <Form.Control type="number" placeholder="XXX-XXX-XXXX" />
-      </FloatingLabel>
-    </>
+    <Container>
+      <Form>
+        <Row>
+          <Col>
+            <FloatingLabel label="First and Last Name">
+              <Form.Control type="text" placeholder="First Name Last Name" />
+            </FloatingLabel>
+          </Col>
+          <Col>
+            <FloatingLabel label="Phone Number" className="mb-3">
+              <Form.Control type="number" placeholder="XXX-XXX-XXXX" />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <FloatingLabel label="Email Address" className="mb-3">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel label="Address" className="mb-3">
+          <Form.Control type="text" placeholder="1234 Main St" />
+        </FloatingLabel>
+        <Row>
+          <Col md={6}>
+            <FloatingLabel label="City" className="mb-3">
+              <Form.Control type="text" placeholder="City Name" />
+            </FloatingLabel>
+          </Col>
+          <Col md={4}>
+            <FloatingLabel label="State" className="mb-3">
+              <Form.Control type="text" placeholder="State Name" />
+            </FloatingLabel>
+          </Col>
+          <Col md={2}>
+            <FloatingLabel label="Zip Code" className="mb-3">
+              <Form.Control type="text" placeholder="Zip Code" />
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <FloatingLabel label="Description">
+          <Form.Control
+            as="textarea"
+            placeholder="Briefly Describe Your Project"
+          />
+        </FloatingLabel>
+      </Form>
+    </Container>
   );
 };
 
