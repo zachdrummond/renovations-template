1d20c6a23bd63c61d8316d575a7e1df384d505f2
Zach Drummond <zachdrummond3@gmail.com>
2021-10-23 17:06:37 -0400

    Added comments to Navigation.jsx

--- src/Containers/Navigation/Navigation.jsx
+++ src/Containers/Navigation/Navigation.jsx
@@ -2,6 +2,8 @@ import { Container, Navbar } from "react-bootstrap";
 import Company from "../../Components/Company/Company";
 import NavLinks from "../../Components/NavLinks/NavLinks";
 
+// SECTION - Navbar
+// SUMMARY - Displays the Navbar Section with the Company Logo and Nav Links
 const Navigation = () => {
   return (
     <Navbar bg="light" variant="light" sticky="top">
