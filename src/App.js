93ec2e3f95a92586b0a66f32229c1c6a52fca2f2
Zach Drummond <zachdrummond3@gmail.com>
2021-10-26 22:07:11 -0400

    Added placeholder image to carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Contact from "./Components/Contact/Contact";
-import Footer from "./Containers/Footer/Footer"
+import Footer from "./Containers/Footer/Footer";
 import Navigation from "./Containers/Navigation/Navigation";
 import Reviews from "./Components/Reviews/Reviews.jsx";
+import Slideshow from "./Components/Slideshow/Slideshow";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Slideshow />
       <Reviews />
       <Contact />
       <Footer />
--- src/Components/Slideshow/Slideshow.jsx
+++ src/Components/Slideshow/Slideshow.jsx
@@ -4,11 +4,16 @@ const Slideshow = () => {
   return (
     <Carousel>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
+        <svg
+          className="bd-placeholder-img"
+          width="100%"
+          height="100%"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+        >
+          <rect width="100%" height="100%" fill="#777"></rect>
+        </svg>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
