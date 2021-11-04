9409e514d41d7fecc0962591bbb5038ee109672a
Zach Drummond <zachdrummond3@gmail.com>
2021-11-03 20:45:16 -0400

    Added portfolio, about, process, contact, and reviews routes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,14 @@ function App() {
       <Navigation />
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path="/renovations-additions" component={/*RenosAndAdditions*/} />
-          <Route exact path="/basements" component={/*Basements*/} />
           <Route exact path="/kitchens" component={/*Kitchens*/} />
+          <Route exact path="/basements" component={/*Basements*/} />
+          <Route exact path="/renovations-additions" component={/*RenosAndAdditions*/} />
+          <Route exact path="/portfolio" component={/*Portfolio*/} />
+          <Route exact path="/about" component={/*About*/} /> 
+          <Route exact path="/process" component={/*Process*/} /> 
+          <Route exact path="/contact" component={/*Contact*/} />
+          <Route exact path="/reviews" component={/*Reviews*/} />
           <Route path="/" component={/*Home*/} />
           <Slideshow />
           <Reviews />
