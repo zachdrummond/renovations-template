d9c21833b5a03ba4a08ca95e0e559e3b30fc1061
Zach Drummond <zachdrummond3@gmail.com>
2021-11-10 10:00:52 -0500

    Added image and text to SidebySideSection

--- src/Components/SideBySideSection/SideBySideSection.jsx
+++ src/Components/SideBySideSection/SideBySideSection.jsx
@@ -1,11 +1,19 @@
+import { Col, Row } from "react-bootstrap";
 // SECTION - HomePage
 // SUMMARY - Displays an image, title, and text split horizontally
-const SideBySideSection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const SideBySideSection = ({ id, title, image, text, direction }) => {
+  return (
+    <section>
+      <Row>
+        <Col>
+          {direction === "left" ? image : direction === "right" ? text : ""}
+        </Col>
+        <Col>
+          {direction === "left" ? text : direction === "right" ? image : ""}
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default SideBySideSection;
--- src/Containers/HomePage/HomePage.jsx
+++ src/Containers/HomePage/HomePage.jsx
@@ -8,7 +8,29 @@ const HomePage = () => {
   return (
     <>
       <HeroCarousel />
-      <SideBySideSection />
+      <SideBySideSection
+        direction="left"
+        image={
+          <svg
+            className="bd-placeholder-img"
+            width="100%"
+            height="100%"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            focusable="false"
+          >
+            <rect width="100%" height="100%" fill="#777"></rect>
+          </svg>
+        }
+        text={
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+            suscipit, consectetur obcaecati commodi vitae soluta quo assumenda
+            cumque explicabo illo architecto, nesciunt aut, doloribus veritatis?
+            Eaque, officia. Sit, nostrum molestias.
+          </p>
+        }
+      />
       <SideBySideSection />
       <SideBySideSection />
       <HowItWorksSection />
