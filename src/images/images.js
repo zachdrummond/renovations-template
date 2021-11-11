39a24a676179080e60be47407c343dd14f112f1d
Zach Drummond <zachdrummond3@gmail.com>
2021-11-11 10:43:26 -0500

    Added placeholder svg to images.js

--- src/images/images.js
+++ src/images/images.js
@@ -1,4 +1,16 @@
 const IMAGES = {
-    YourLogoHere: require("./YourLogoHere.png").default,
-}
+  YourLogoHere: require("./YourLogoHere.png").default,
+  placeholder: (
+    <svg
+      className="bd-placeholder-img"
+      width="100%"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <rect width="100%" height="100%" fill="#777"></rect>
+    </svg>
+  ),
+};
 export default IMAGES;
