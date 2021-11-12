1a46d030b69ed173a9b68c72f17341abef74cb15
Zach Drummond <zachdrummond3@gmail.com>
2021-11-12 09:30:17 -0500

    Added title and text to PortfolioSection

--- src/Components/PortfolioSection/PortfolioSection.jsx
+++ src/Components/PortfolioSection/PortfolioSection.jsx
@@ -1,12 +1,17 @@
 import { Button, Card, Container, Stack } from "react-bootstrap";
 
-const PortfolioSection = () => {
-    return (
-        <Container className="my-5 text-center">
-
-            
-        </Container>
-    );
+const PortfolioSection = ({title}) => {
+  return (
+    <Container className="my-5 text-center">
+      <h1>{title} Portfolio</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo neque
+        voluptas error quae voluptatem veritatis, tempore, minus iure expedita
+        provident adipisci! Molestiae quos pariatur consectetur excepturi
+        quisquam fugit facilis odio.
+      </p>
+    </Container>
+  );
 };
 
 export default PortfolioSection;
