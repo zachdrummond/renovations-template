6f9009b5f9a653be02fe3d04b9d7828fe5cc3050
Zach Drummond <zachdrummond3@gmail.com>
2021-11-13 19:21:16 -0500

    Updated AboutPage

--- src/Containers/AboutPage/AboutPage.jsx
+++ src/Containers/AboutPage/AboutPage.jsx
@@ -1,8 +1,13 @@
+import IMAGES from "../../images/images";
+import HeroCarousel from "../../Components/HeroCarousel/HeroCarousel";
+import SideBySideSection from "../../Components/SideBySideSection/SideBySideSection";
+import ReviewsSection from "../../Components/ReviewsSection/ReviewsSection";
+
 const AboutPage = () => {
     return (
-        <div>
+        <main>
             <h1>About</h1>
-        </div>
+        </main>
     );
 };
 
