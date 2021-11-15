a76b3ed38755dc1a790b5248b393a4e83b272ed0
Zach Drummond <zachdrummond3@gmail.com>
2021-11-15 10:42:01 -0500

    Turned CompanyLogo into a link

--- src/Components/CompanyLogo/CompanyLogo.jsx
+++ src/Components/CompanyLogo/CompanyLogo.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import IMAGES from "../../images/images";
 
 // SECTION - Navbar & Footer
 // SUMMARY - Displays the company logo
 const CompanyLogo = () => {
   return (
-    <img
-      className="img-fluid"
-      alt="Your Logo Here"
-      src={IMAGES.YourLogoHere}
-      width="150"
-    />
+    <Link to="/">
+      <img
+        className="img-fluid"
+        alt="Your Logo Here"
+        src={IMAGES.YourLogoHere}
+        width="150"
+      />
+    </Link>
   );
 };
 
