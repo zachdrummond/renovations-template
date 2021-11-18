922f39b1020faa5daf292215e2054a392d239e13
Zach Drummond <zachdrummond3@gmail.com>
2021-11-18 15:12:30 -0500

    Added Container, h1, and p to PricesSection

--- src/Components/PricesSection/PricesSection.jsx
+++ src/Components/PricesSection/PricesSection.jsx
@@ -1,9 +1,15 @@
 const PricesSection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Container className="my-5 text-center">
+      <h1>{title} Prices</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+        optio dolore nihil quae impedit molestias, nostrum deleniti quo ducimus
+        rem voluptas repellat. Aliquam amet, dicta excepturi corporis rem
+        dolorem! Iste.
+      </p>
+    </Container>
+  );
 };
 
 export default PricesSection;
