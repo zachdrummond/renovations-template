04817b00664325cabeddb411c9534f4ef51f1804
Zach Drummond <zachdrummond3@gmail.com>
2021-11-18 15:15:29 -0500

    Added cards with images and text to PricesSection

--- src/Components/PricesSection/PricesSection.jsx
+++ src/Components/PricesSection/PricesSection.jsx
@@ -1,3 +1,5 @@
+import { Button, Card, Container, Stack } from "react-bootstrap";
+
 const PricesSection = () => {
   return (
     <Container className="my-5 text-center">
@@ -8,6 +10,40 @@ const PricesSection = () => {
         rem voluptas repellat. Aliquam amet, dicta excepturi corporis rem
         dolorem! Iste.
       </p>
+      <Stack direction="horizontal" gap={3} className="mb-4">
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Phone Consultation</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              ratione beatae explicabo iusto eaque soluta vel eos magnam
+              mollitia et, molestias, nostrum enim ullam veniam vitae excepturi
+              dignissimos dolorem fugit?
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Design and Renovation</Card.Title>
+            <Card.Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              ratione beatae explicabo iusto eaque soluta vel eos magnam
+              mollitia et, molestias, nostrum enim ullam veniam vitae excepturi
+              dignissimos dolorem fugit?
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>{card3Title}</Card.Title>
+            <Card.Text>{card3Text}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Stack>
+      <Button variant="outline-primary">Learn More About Our Process</Button>
     </Container>
   );
 };
